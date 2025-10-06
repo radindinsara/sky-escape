94f23558f48cb278971433b2bbbeb4b8d1f71475
Nathan <nathansgirma@gmail.com>
2025-10-06 12:02:00 +0300

    Extended project

--- GameEngine.py
+++ GameEngine.py
@@ -1,6 +1,6 @@
 from datetime import time
 
-from Queries import( register_player)
+from Queries import( register_player, log_action)
 
 class SkyEscapeGame:
     def __init__(self, player_name, role):
@@ -23,3 +23,15 @@ class SkyEscapeGame:
 
             move_ident = input("\nEnter the airport you want to move to: ").strip().upper()
             selected_airport = next((a for a in self.airports if a['ident'].upper() == move_ident), None)
+            if not selected_airport:
+                print("❌ Invalid IDENT. Try again.")
+                continue
+
+            if selected_airport['ident'] in self.visited_by_player:
+                print("⚠️ You already visited this airport. Choose another one.")
+                continue
+
+            self.player_airport = selected_airport
+            self.visited_by_player.add(selected_airport['ident'])
+            print(f"\nYou moved to: {self.player_airport['name']} ({self.player_airport['ident']})")
+            log_action(self.game_id, f"Player moved to {self.player_airport['name']} ({self.player_airport['ident']})")
--- Queries.py
+++ Queries.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import DBconnection as db
 
 
@@ -20,3 +22,16 @@ def register_player(name, role):
 
     conn.close()
     return player_id
+
+def log_action(game_id, description):
+    """Log every move or event."""
+    conn = db.get_db()
+    if not conn:
+        return
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO game_actions (game_id, action_time, description)
+        VALUES (%s, %s, %s)
+    """, (game_id, datetime.datetime.now(), description))
+    conn.commit()
+    conn.close()

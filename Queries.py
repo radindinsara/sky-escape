4e5576ea29bcfe6f2f8d5dae38a5bdc66ab4319c
akila <ashashimantha@gmail.com>
2025-10-06 11:52:02 +0300

    class def and query update new

--- Queries.py
+++ Queries.py
@@ -6,5 +6,17 @@ def register_player(name, role):
     if not conn:
         return None
     cursor = conn.cursor(dictionary=True)
+
+    # Check if player already exists
     cursor.execute("SELECT * FROM players WHERE name = %s", (name,))
     player = cursor.fetchone()
+
+    if player:
+        player_id = player["id"]
+    else:
+        cursor.execute("INSERT INTO players (name, role) VALUES (%s, %s)", (name, role))
+        conn.commit()
+        player_id = cursor.lastrowid
+
+    conn.close()
+    return player_id

d43f2063ce16054d900f706ccc34f9d3b9ccebc8
Radin Dinsara <radinpersonald@gmail.com>
2025-12-02 01:18:39 +0200

    Add homepage with name input and start button

---
+++ JS/home.js
@@ -0,0 +1,12 @@
+var playerName = "";
+
+function startGame() {
+    playerName = document.getElementById("nameInput").value;
+
+    if(playerName == "") {
+        alert("Please enter your name!");
+        return;
+    }
+
+    alert("Welcome " + playerName + "!");
+}

4c1acd1e053ed409cff3f2cd16cbfe1c7ae38e45
akila <ashashimantha@gmail.com>
2025-12-04 10:46:39 +0200

    Edit score logic again

---
+++ Akila M3/score.js
@@ -0,0 +1,42 @@
+// =============================
+// THIEF LOGIC (if thief IDs exist)
+// =============================
+const thiefScoreText = document.getElementById("thief-score");
+const thiefInput = document.getElementById("thiefInput");
+
+let thiefScore = 0;
+
+if (thiefScoreText && thiefInput) {
+  thiefInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+
+      thiefScore += 10;
+
+      thiefScoreText.textContent = "Score: " + thiefScore;
+
+      thiefInput.value = "";
+    }
+  });
+}
+
+
+// =============================
+// POLICE LOGIC (if police IDs exist)
+// =============================
+const policeScoreText = document.getElementById("police-score");
+const policeInput = document.getElementById("policeInput");
+
+let policeScore = 250;
+
+if (policeScoreText && policeInput) {
+  policeInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+
+      policeScore -= 10;
+
+      policeScoreText.textContent = "Score: " + policeScore;
+
+      policeInput.value = "";
+    }
+  });
+}
